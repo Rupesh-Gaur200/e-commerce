f8415635d348595d8b71b9cab591e7199be5f19b
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-10-16 11:50:09 +0530

    making navigation bar responsive  layout

--- ecom1/src/Component/Navbar.jsx
+++ ecom1/src/Component/Navbar.jsx
@@ -1,42 +1,34 @@
+import { Link  , Outlet} from "react-router-dom";
 
-import { Link , Outlet } from "react-router-dom";
-function Navbar(){
-    return(
-        <>
-        
-    
-            <nav className=" w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-               <div>
-         <ul className='flex gap-8'>
-             <li className='hover:scale-125 duration-300 cursor-pointer'>
-                <Link to ="/Home">Home</Link>
-             </li>
-             <li className='hover:scale-125 duration-300 cursor-pointer'>
-                <Link to ="/Trending">Trending</Link>
-             </li>
-
-             <li className='hover:scale-125 duration-300 cursor-pointer'>
-                      <Link to="/Allbogs">All bogs</Link>
-
-             </li>
-
-             <li className='hover:scale-125 duration-300 cursor-pointer'>
-                <Link to ="/Create">Create</Link>
-             </li>
-
-
-
+function Navbar() {
+  return (
+    <>
+      <nav className="bg-[#0a192f] text-gray-300 ">
+        <div className="container mx-auto px-4 py-4 md:py-0 flex justify-between items-center w-full h-[80px] ">
+          <ul className="flex gap-8">
+            <li className="hover:scale-125 duration-300 cursor-pointer">
+              <Link to="/Home">Home</Link>
+            </li>
+            <li className="hover:scale-125 duration-300 cursor-pointer">
+              <Link to="/Trending">Trending</Link>
+            </li>
+            <li className="hover:scale-125 duration-300 cursor-pointer">
+              <Link to="/Allbogs">All Blogs</Link>
+            </li>
+            <li className="hover:scale-125 duration-300 cursor-pointer">
+              <Link to="/Create">Create</Link>
+            </li>
           </ul>
-          </div>
-          
-          </nav>
-       
-        <main>
-                <Outlet></Outlet>
-             </main> 
-             
-        </>
-    )
+        </div>
+      </nav>
+
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </>
+  );
 }
+
 export default Navbar;
+
 
