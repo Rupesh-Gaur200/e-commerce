1dae7a5a522eaca12bacc1e8e2f85b801ec17483
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-10-16 11:11:16 +0530

    done with seting up react-router-dom

--- ecom1/src/App.jsx
+++ ecom1/src/App.jsx
@@ -1,11 +1,29 @@
+import {
+
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider} from 'react-router-dom'
+
+
+import Navbar from './Component/Navbar'
+
+
 
 
 function App() {
+  const router=createBrowserRouter(createRoutesFromElements(
+    <Route element={<Navbar/>}>
+       <Route path='/' element={<Navbar/>}/>
+      
+
+    </Route>
+  ))
  
 
   return (
     <>
-      <h1 className="">hello react </h1>
+      <RouterProvider router={router}/> 
     </>
   )
 }
---
+++ ecom1/src/Component/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+
+function Navbar(){
+    return (
+        <>
+           <h1>Navbar</h1>
+        </>
+    )
+}
+
+export default Navbar
